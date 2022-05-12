fbc6af7a184f4c7413bfe9862668777f001d05c9
Aishwarya K <aishwaryak@infoblox.com>
2022-05-12 19:16:08 +0530

    UI cleanup

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -7,16 +7,12 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'design_app', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'education_atom', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'location_map-big', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'ui-1_bell-53', class: '' },
+    { path: '/dashboard', title: 'Dashboard',  icon: '', class: '' },
+    { path: '/icons', title: 'Data Management',  icon:'', class: '' },
+    { path: '/maps', title: 'Smart Folders',  icon:'', class: '' },
+    { path: '/notifications', title: 'Grid',  icon:'', class: '' },
 
-    { path: '/user-profile', title: 'User Profile',  icon:'users_single-02', class: '' },
-    { path: '/table-list', title: 'Table List',  icon:'design_bullet-list-67', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'text_caps-small', class: '' },
-    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'objects_spaceship', class: 'active active-pro' }
-
+    { path: '/user-profile', title: 'Administrations',  icon:'', class: '' }
 ];
 
 @Component({
